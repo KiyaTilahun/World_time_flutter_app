b93223e359dc9ca55fc08d7c6940395aad1db3fe
kiyatilahun <kiyatilahun0@gmail.com>
2023-08-17 17:47:15 +0300

    started my first project in flutter

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home: Mycard
+()));
+
+class Mycard extends StatelessWidget {
+  const Mycard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
