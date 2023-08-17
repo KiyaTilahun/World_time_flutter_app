ede41819148731435970c68c66857ffb259b643c
kiyatilahun <kiyatilahun0@gmail.com>
2023-08-17 18:56:46 +0300

    master

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-;
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables;
 import 'package:flutter/material.dart';
-void main(List<String> args) {
-  return runApp(home:MaterialApp())
+
+void main(List<String> args) => runApp( MaterialApp(home:Quotes()));
+
+class Quotes extends StatefulWidget {
+  const Quotes({super.key});
+
+  @override
+  State<Quotes> createState() => _QuotesState();
+}
+
+class _QuotesState extends State<Quotes> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
 }
